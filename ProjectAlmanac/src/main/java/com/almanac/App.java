d03190a120cf4a2acb97dce27067bac04625745d
cpibidevelopment <cpi.bi.development@gmail.com>
2017-09-20 18:32:44 +0800

    Initial commit for ProjectAlmanac

---
+++ ProjectAlmanac/src/main/java/com/almanac/App.java
@@ -0,0 +1,13 @@
+package com.almanac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
